b4d8f8f97599f8de129658b9a71455039c88763c
andres fernando mena <afmg19791130@gmail.com>
2025-02-25 15:49:53 -0500

    agregar comentarios

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ let stringDiez = '10' ;
 
 
 
-if (numeroUn == stringUn) {
+if (numeroUn == stringUn) {     // == en JavaScript convierte automáticamente '1' en 1 antes de compararlos.
 
     console.log('Las variables numeroUn y stringUn tienen el mismo valor, pero tipos diferentes') ;
 
@@ -22,7 +22,7 @@ if (numeroUn == stringUn) {
 
 
 
-if ( numeroTreinta === stringTreinta) {
+if ( numeroTreinta === stringTreinta) {    // === en JavaScript solo compara los mismos tipos de datos, por lo que será false, y se ejecutará el else, que es el resultado correcto.
 
   console.log('Las variables numeroTreinta y stringTreinta tienen el mismo valor y el mismo tipo')
 
